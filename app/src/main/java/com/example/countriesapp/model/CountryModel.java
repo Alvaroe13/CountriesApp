9e17dc30e19d1176ac3e96a613c65181fd265bb4
Alvaro Isea <alvaroe9448@gmail.com>
2020-05-03 20:36:05 +0200

    *added some value in the Model *added value in the ViewModel

--- app/src/main/java/com/example/countriesapp/model/CountryModel.java
+++ app/src/main/java/com/example/countriesapp/model/CountryModel.java
@@ -1,4 +1,25 @@
 package com.example.countriesapp.model;
 
 public class CountryModel {
+    String name;
+    String capital;
+    String flag;
+
+    public CountryModel(String name, String capital, String flag) {
+        this.name = name;
+        this.capital = capital;
+        this.flag = flag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
 }
--- app/src/main/java/com/example/countriesapp/viewmodel/ListCountryModel.java
+++ app/src/main/java/com/example/countriesapp/viewmodel/ListCountryModel.java
@@ -1,4 +1,42 @@
 package com.example.countriesapp.viewmodel;
 
-public class ListCountryModel {
+import com.example.countriesapp.model.CountryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListCountryModel extends ViewModel {
+
+    public MutableLiveData<List<CountryModel>> countriesList = new MutableLiveData<List<CountryModel>>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loadingError = new MutableLiveData<Boolean>();
+
+    //this is UI entry point to the ViewModel
+    public void refresh(){
+        fetchInfoFromDB();
+    }
+
+    //here's where the fetching from the server happens to be pass to the UI in refresh method
+    private void fetchInfoFromDB() {
+        //fake info for now
+        CountryModel country1 = new CountryModel("Argentina", "Buenos Aires", "");
+        CountryModel country2 = new CountryModel("Bolivia", "La Paz", "");
+        CountryModel country3 = new CountryModel("Colombia", "Bogota", "");
+
+        List<CountryModel> listOfCountries = new ArrayList<>();
+        listOfCountries.add(country1);
+        listOfCountries.add(country2);
+        listOfCountries.add(country3);
+
+        countriesList.setValue( listOfCountries );
+        //these next two are false since we're not fetching info from a server just yet.
+        loading.setValue(false);
+        loadingError.setValue(false);
+
+    }
+
+
 }
